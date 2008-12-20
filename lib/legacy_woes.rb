5ecbaa63e82b3a9b140b4da5259ed67171f0d8e0
Amos King <amos.l.king@gmail.com>
2008-12-19 21:54:32 -0600

    add initial rdoc

--- lib/legacy_woes.rb
+++ lib/legacy_woes.rb
@@ -1,9 +1,18 @@
+# Get rid of some of the legacy database woes
+#
+# Author::    Amos King  (mailto:amos.l.king@gmail.com)
+# Copyright:: Copyright (c) 2008 Amos King
+# License::   Released under the MIT license
+
+
 module LegacyWoes
   def self.included(base)
     base.extend ClassMethods
   end
   
   module ClassMethods
+    # Sets the created_at column.
+    # Also defines methods for created_at and created_at=.
     def created_at_column(legacy_column_name)
       module_eval <<-EOB
         def write_attribute_with_#{legacy_column_name}_created_at(name, value, &block)
@@ -25,6 +34,8 @@ module LegacyWoes
       alias_method_chain :write_attribute, "#{legacy_column_name}_created_at"
     end
     
+    # Sets the updated_at column.
+    # Also defines methods for updated_at and updated_at=.
     def updated_at_column(legacy_column_name)
       module_eval <<-EOB
         def write_attribute_with_#{legacy_column_name}_updated_at(name, value, &block)
