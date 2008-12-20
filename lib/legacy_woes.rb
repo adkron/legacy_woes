315619a33fb390301a76b807959c5652d29a2d40
Amos King <amos.l.king@gmail.com>
2008-12-19 21:49:45 -0600

    did a little refactoring and added updated at

--- lib/legacy_woes.rb
+++ lib/legacy_woes.rb
@@ -5,23 +5,45 @@ module LegacyWoes
   
   module ClassMethods
     def created_at_column(legacy_column_name)
-      alias_method_chain :write_attribute, "#{legacy_column_name}_created_at"
-      
-      define_method("write_attribute_with_#{legacy_column_name}_created_at", name, value, &block) do
-        if name == 'created_at'
-          write_attribute('datesubmitted', value)
-        else
-          send("write_attribute_without_#{legacy_column_name}_created_at", name, value, &block)
+      module_eval <<-EOB
+        def write_attribute_with_#{legacy_column_name}_created_at(name, value, &block)
+          if name == 'created_at'
+            write_attribute('#{legacy_column_name}', value)
+          else
+            send(:write_attribute_without_#{legacy_column_name}_created_at, name, value, &block)
+          end
         end
-      end
-      
-      define_method(:created_at) do
+        EOB
+         
+      define_method :created_at do
         send(legacy_column_name)
       end
-      
-      define_method(:created_at=, value) do
-        send("#{legacy_column_name}=", value)
+    
+      define_method :created_at= do |value|
+        send('#{legacy_column_name}=', value)
       end
+      alias_method_chain :write_attribute, "#{legacy_column_name}_created_at"
+    end
+    
+    def updated_at_column(legacy_column_name)
+      module_eval <<-EOB
+        def write_attribute_with_#{legacy_column_name}_updated_at(name, value, &block)
+          if name == 'updated_at'
+            write_attribute('#{legacy_column_name}', value)
+          else
+            send(:write_attribute_without_#{legacy_column_name}_updated_at, name, value, &block)
+          end
+        end
+        EOB
+         
+      define_method :updated_at do
+        send(legacy_column_name)
+      end
+    
+      define_method :updated_at= do |value|
+        send('#{legacy_column_name}=', value)
+      end
+      alias_method_chain :write_attribute, "#{legacy_column_name}_updated_at"
     end
   end
 end
